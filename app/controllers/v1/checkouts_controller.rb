8b50e1ba84fa945a83307cd9471c4a02f12fe438
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-14 15:18:14 -0500

    Add routes and public index files

--- app/controllers/v1/checkouts_controller.rb
+++ app/controllers/v1/checkouts_controller.rb
@@ -1,2 +1,20 @@
 class V1::CheckoutsController < ApplicationController
+  
+  def new
+    @client_token = gateway.client_token.generate
+  end
+
+  def show
+    @transaction = gateway.transaction.find(params[:id])
+    @result = _create_result_hash(@transaction)
+  end
+
+  def gateway
+    @gateway = Braintree::Gateway.new(
+      :environment => :sandbox,
+      :merchant_id => ENV['MERCHANT_ID'],
+      :public_key => ENV['PUBLIC_KEY'],
+      :private_key => ENV['PRIVATE_KEY'],
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    
+
+    resources :checkouts, only: [:new, :create, :show]
 
   end
 end
