9bcf5d9e1454a6f43dc3ab899415178c39e8cd6a
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-15 16:16:28 -0500

    Add create method to checkouts controller

--- app/controllers/v1/checkouts_controller.rb
+++ app/controllers/v1/checkouts_controller.rb
@@ -5,6 +5,25 @@ class V1::CheckoutsController < ApplicationController
     render json: client_token
   end
 
+  def create
+    nonce = params["payment_method_nonce"]
+
+    result = gateway.transaction.sale(
+      amount: 13.00,
+      payment_method_nonce: nonce,
+      :options => {
+        :submit_for_settlement => true
+      }
+    )
+    if result.success? || result.transaction
+      redirect_to checkout_path(result.transaction.id)
+    else
+      error_messages = result.errors.map { |error| "Error: #{error.code}: #{error.message}" }
+      flash[:error] = error_messages
+      redirect_to new_checkout_path
+    end
+  end
+
   def gateway
     @gateway = Braintree::Gateway.new(
       :environment => :sandbox,
